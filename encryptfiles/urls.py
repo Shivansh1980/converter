0d68da6d6a94141a6e006f7743d3626d62eea038
Shivansh1980 <shivanshshrivastava1980@gmail.com>
2021-01-04 21:51:30 +0530

    ajax form added

--- encryptfiles/urls.py
+++ encryptfiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import encrypthomepage
+from .views import encrypthomepage, fileEncryptGetRequest
 urlpatterns = [
     path('', encrypthomepage, name='encryptfilehomepage'),
-    path('file-encrypt',encrypthomepage, name='encryptfile')
+    #path('file-encrypt/', encrypthomepage, name='encryptfile')
+    path('file-encrypt/', fileEncryptGetRequest, name='encryptfile')
 ]
--- encryptfiles/views.py
+++ encryptfiles/views.py
@@ -2,47 +2,83 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 import os
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 from .ConverterTools import write_key, load_key, encryptFile, decryptFile
 from .models import EncryptedFile
 from converter import settings
-
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from django.core.serializers import serialize
+from .forms import EncryptedFileForm
 
 path_to_media = settings.MEDIA_ROOT
+API_KEY = 1
+
 
 # Create your views here.
 def encrypthomepage(request):
-    param = {'file': ''}
-    username = ""
+    response_data = {'data': None, 'msg': None,  'downloadurl': None,'form':None}
+    # if (request.method == "POST"):
+    #     try:
+    #         file = request.FILES['myfile']
+    #         useremail = request.POST['email']
+    #     except:
+    #         response_data['msg'] = "File Not Exist Try Reuploading"
+    #         return render('encryption/encryptionhome.html')
+
+    #     fs = FileSystemStorage()
+    #     fs.save(file.name, file)
+    #     write_key()
+    #     k = load_key()
+
+    #     filename = file.name
+    #     path = path_to_media+filename
+
+    #     encryptFile(path, k)
+
+    #     output = file.name
+    #     # this is the url which we return as json response
+    #     x = str(f"/static/media/{output}")
+
+    #     encryptfile = EncryptedFile(
+    #         useremail=useremail,
+    #         filename=filename,
+    #         key=k,
+    #         file=file,
+    #         url=x
+    #     )
+    #     encryptfile.save()
+
+    #     print("File Path : "+fs.location)
+    #     print("File Name : ", file.name)
+    #     response_data['downloadurl'] = x
+    #     response_data['msg'] = "Successfully Encrypted Your File"
+    form = EncryptedFileForm()
+    response_data['form'] = form
+    return render(request, 'encryption/encryptionhome.html',response_data)
+
+def decryptTheFile(requesst):
+    response_data = {'data': None, 'msg': None, 'downloadurl': None}
+    
+    
+@csrf_exempt
+def fileEncryptGetRequest(request):
+    result = {'msg': '','error':None,'errors':''}
     if (request.method == "POST"):
-        try:
-            file = request.FILES['myfile']
-            useremail = request['email']
+        print(request.POST)
+        form = EncryptedFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            result['msg'] = 'File Encryption Successfull'
+            result['error'] = False
+            return JsonResponse(result)
+        else:
+            result['msg'] = "Wrong Input Provided"
+            result['error'] = True
+            result['errors'] = form.errors
+            return JsonResponse(result)
+    else:
+        return JsonResponse({'msg':'Request Was Not POST'})
 
-        except:
-            return render(request, 'encryption/encryptionhome.html', param)
-        fs = FileSystemStorage()
-        write_key()
-        k = load_key()
-        filename = file.name
-        path = path_to_media+"outputs\\"+filename
-        print("Destination Path : ", path)
-        
-        output = file.name
-        x = str(f"/static/media/outputs/{output}")
-        encryptFile()
-        encryptfile = EncryptedFile(
-            useremail = username,
-            filename=filename,
-            key=k,
-            file=file,
-            url=x
-        )
-        encryptfile.save()
-
-        print("File Path : "+fs.location)
-        print("File Name : ",file.name)
-        param['file'] = x
-        print(param['file'])
-        return render(request, 'encryption/encryptionhome.html', param)
-    return render(request, 'encryption/encryptionhome.html', param)
