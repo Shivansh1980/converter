d8e23e68a33fd6e298b765d9b243478ec998a0a3
Shivansh1980 <shivanshshrivastava1980@gmail.com>
2021-04-22 10:57:30 +0530

    ready to host

--- converter/settings.py
+++ converter/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['encryptor-api.herokuapp.com', "localhost","127.0.0.1"]
 #you have defined this hostname to provide the download url using ajax
