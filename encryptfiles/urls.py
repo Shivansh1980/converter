7a33a80f4277a01970bae136c7c4bad012ef8701
Shivansh1980 <shivanshshrivastava1980@gmail.com>
2021-04-23 23:51:11 +0530

    url fixed

--- encryptfiles/urls.py
+++ encryptfiles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import encrypt_homepage, encrypt_file_request, delete_all_files, delete_file, is_file_exists, decrypt_file_request
 urlpatterns = [
     path('', encrypt_homepage, name='encryptfilehomepage'),
-    path('file_encrypt/', encrypt_file_request, name='encryptfile'),
+    path('encrypt_file/', encrypt_file_request, name='encryptfile'),
     path('decrypt_file/', decrypt_file_request, name='decryptfile'),
     path('delete_all_files/',delete_all_files, name='deletefiles'),
     path('is_file_exists/<str:key>', is_file_exists, name='isfileexists'),
