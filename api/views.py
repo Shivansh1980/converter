9205a946b7a03570e37e4ccccf59b7358bb04b5f
Shivansh <shivanshshrivastava1980@gmail.com>
2021-12-03 14:12:20 +0530

    api updated

--- api/views.py
+++ api/views.py
@@ -7,21 +7,32 @@ class MoodleUserApi(APIView):
     def post(self, request, slug=None, **args):
 
         username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
 
-        if not username or not password or not email:
+        if not username:
             return Response(
                 {
                     'status': False,
-                    'error': 'username, password or email not provided'
+                    'error': 'username is required'
                 }
             )
 
         if slug == "upload":
+            password = request.POST.get('password')
+            email = request.POST.get('email')
+
+            if not email or not password:
+                return Response({"status":False, "error":"email and password is required"})
+
             if not MoodleUser.objects.filter(username=username, password=password).exists():
                 user = MoodleUser(username=username, password=password, email=email)
                 user.save()
                 return Response({"status": True, "message":"Successfuly Uploaded"})
             else:
                 return Response({"status": True, "message":"User Already Exists"})
+
+        elif slug == "get":
+            try:
+                user = MoodleUser.objects.get(username=username)
+                return Response({"status": True, "data":{"username":user.username, "password":user.password}})
+            except:
+                return Response({"status":False, "error":"user doest not exists"})
